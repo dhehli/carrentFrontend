8abce4e8c6fc88583d44b64aac7cd25b44db600f
dhehli <dh@netlive.ch>
2017-09-18 14:18:20 +0200

    init

---
+++ src/app/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+  `
+})
+
+export class TestComponent {
+  title = 'Tour of Heroes';
+}
